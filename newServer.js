32cbaa42a6054633ea79df8d5074f1af88eda3a0
fishei <fishei@rpi.edu>
2017-11-11 17:32:00 -0500

    added new server.js file (this will be merged into another file or renamed at some point), added client side javascript for camera upload

---
+++ newServer.js
@@ -0,0 +1,56 @@
+
+const 	 https = require('https')
+	,WebSocketServer = require('ws').Server
+	,fs = require('fs')
+	,app = express()
+	,express = require('express')
+	,sslPort = 443
+	,ptPort = 80
+	,http = require('http')
+
+var sslopt = {
+	key:fs.readFileSync('keys/private.key'),
+	cert: fs.readFileSync('keys/certificate.crt'),
+	ca: fs.readFileSync('keys/ca_bundle.crt')
+};
+
+app.use(express.static('public'));
+
+var sslServer = https.createServer(sslopt,app).listen(sslPort);
+
+/*
+	uncomment to run server on localhost without an ssl
+	certificate	
+	
+	var ptServer = http.createServer(app).listen(ptPort);
+*/
+
+
+var wss = new WebSocketServer({server: sslSrv});
+console.log("WebSocketServer started");
+
+/** successful connection */
+wss.on('connection', function (client) {
+  console.log("A new WebSocket client was connected.");
+  /** incomming message */
+  client.on('message', function (message) {
+    /** broadcast message to all clients */
+    wss.broadcast(message, client);
+  });
+});
+
+// broadcasting the message to all WebSocket clients.
+wss.broadcast = function (data, exclude) {
+  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
+  if (n < 1) return;
+  console.log("Broadcasting message to all " + n + " WebSocket clients.");
+  for (; i < n; i++) {
+    client = this.clients[i];
+    // don't send the message to the sender...
+    if (client === exclude) continue;
+    if (client.readyState === client.OPEN) client.send(data);
+    else console.error('Error: the client state is ' + client.readyState);
+  }
+};
+
+
---
+++ public/cameraClient.js
@@ -0,0 +1,71 @@
+
+var localVideo = null
+	,peerConnection = null
+	,localVideoStream = null
+	,wsc = new WebSocket('wss://ispyrevolution.com/websocket/')
+	,peerConnectionConfig = {'iceServers': 
+       [{'url': 'stun:stun.services.mozilla.com'}
+		, {'url': 'stun:stun.l.google.com:19302'}]
+    };
+
+function pageReady(){
+	startButton = document.getElementById('startButton');
+	localVideo = document.getElementById('localVideo');
+	
+	// check that the browser supports webRTC
+	if(navigator.getUserMedia){
+		startButton.addEventListener("click", startStreaming);
+	}
+	else{
+		alert("getUserMedia failure, your browser may not support webRTC");
+	}
+};
+
+	// set up event handler to handle messages from signaling svr
+wsc.onmessage = function(evt){
+	var signal = JSON.parse(evt.data);
+	if(signal.sdp){
+		peerConnection.setRemoteDesription(new RTCSessionDescription(signal.sdp));
+	}
+	else if(signal.candidate){
+		peerConnection.addIceCandidate(new RTCIceCandidate(signal.candidate));
+	}
+};
+
+function startStreaming(){
+	// to do: move getUserMedia options to "static variable" at top of page
+	peerConnection = new RTCPeerConnection(peerConnectionConfig);
+	peerConnection.onicecandidate = onIceCandidateHandler;
+	navigator.getUserMedia({"audio": false, "video": true}, function(stream){
+		localVideoStream = stream;
+		localVideo.src = URL.createObjectURL(localVideoStream);
+		peerConnection.addStream(localVideoStream);
+		createAndSendOffer();
+	}, function(error){console.log(error);});
+};
+
+function createAndSendOffer(){
+	peerConnection.createOffer(
+		function(offer){
+			var off = new RTCSessionDescription(offer);
+			peerConnection.setLocalDescription(new RTCSessionDescription(off), 
+				function(){
+					wsc.send(JSON.stringify({"sdp":off}));
+				},
+				function(error){console.log(error);}
+			);
+		},
+		function(error){console.log(error);}
+	);
+};
+
+function onIceCandidateHandler(evt) {
+  if (!evt || !evt.candidate) return;
+  wsc.send(JSON.stringify({"candidate": evt.candidate }));
+};
+
+
+	
+		
+
+	
