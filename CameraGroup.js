cca60e103b1ed9adc22c727f40a958672d3f654a
fishei <fishei@rpi.edu>
2017-12-04 15:10:04 -0500

    added classes to handle client connections on the server side

---
+++ CameraGroup.js
@@ -0,0 +1,42 @@
+
+
+function CameraGroup(newGroupId, cameraSock){
+	var groupId = newGroupId;
+	var viewingClients = [];
+
+	// ClientConnection object representing the camera uploading video
+	var cameraClient = new CameraClient(groupId,this,cameraSock);
+
+	this.addViewer = function(viewerSock){
+		console.log('New viewing client added to ' + this.description());
+		viewingClients.push(new ViewingClient(groupId,this,viewerSock));
+		console.log('Sending ack message to new viewing client in ' + this.description());
+		viewerSock.send(JSON.stringify({"connected":true}));
+	};
+
+	this.description = function(){ return 'CameraGroup ' + groupId;};
+
+	this.onCameraMessage = function(message){
+		console.log(this.description() + ' received message from camera client');
+		broadcastToViewers(message);
+	};
+
+	this.broadcastToViewers = function(message){
+		console.log(this.description() + ' broadcasting message to viewers');
+		viewingClients.foreach(function(item,index,array){
+			item.sendMessage(message);
+		});
+	};
+	
+	
+	this.onViewerMessage = function(message){
+		console.log(this.description() + ' received message from viewing client');
+		console.log(this.description() + ' sending message to camera client');
+		cameraClient.sendMessage(message);
+	};
+
+	this.onMessage = function(message, isCamera){
+		if(isCamera) onCameraMessage(message);
+		else onViewerMessage(message);
+	};
+}
---
+++ ClientConnection.js
@@ -0,0 +1,38 @@
+
+
+function ClientConnection(newGroupId, newParent, newWebSock){
+	var groupId = newGroupId;
+	var parent = newParent;
+	var webSock = newWebSock;
+
+	this.isCamera = function(){
+		return false;
+	};
+
+	this.getGroupId = function(){
+		return groupId;
+	};
+
+	// add message handler function onMessage
+	webSock.on('message', onMessage);
+
+	// to do: add error handling, check for closed web sockets
+	this.sendMessage = function(message){
+		webSock.send(message);
+	};
+}
+
+ClientConnection.prototype.onMessage = function(message){
+	parent.onMessage(message, this.isCamera());
+};
+
+function CameraClient(newGroupId, newParent, newWebSock){
+	ClientConnection.call(this,newGroupId, newParent, newWebSock);
+};
+
+CameraClient.prototype = Object.create(CameraConnection.prototype);
+CameraClient.prototype.constructor = CameraClient;
+
+CameraClient.prototype.isCamera = function(){
+	return true;
+}
