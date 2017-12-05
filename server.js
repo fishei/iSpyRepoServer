b6ae4927108d0957783890aca50a644dc10c0ed8
segalj <segalj@rpi.edu>
2017-12-05 15:57:08 -0500

    added http->https forwarding

--- server.js
+++ server.js
@@ -19,3 +19,20 @@ var sslopt = {
 app.use(express.static('public'))
 
 var server = https.createServer(sslopt,app).listen(443);
+
+
+//credit to stack overflow user jake for http forwarding https://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+
+// set up plain http server
+var http = express.createServer();
+
+// set up a route to redirect http to https
+http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
+// have it listen on 80
+http.listen(80);
