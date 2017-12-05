daea760a4f421adf90b741e466029eda5888576b
segalj <segalj@rpi.edu>
2017-12-05 16:12:30 -0500

    added forwarding

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ var webSocketServerOpts = {
 	clientTracking: true
 }
 
+
+// set up plain http server
+var forward = express.createServer();
+
+// set up a route to redirect http to https
+forward.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
+// have it listen on 80
+forward.listen(80);
+
 /*
 	uncomment to run server on localhost without an ssl
 	certificate	
