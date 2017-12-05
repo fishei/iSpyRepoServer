94219b0c6bb0886172dccce2d1c64685f0b8949e
segalj <segalj@rpi.edu>
2017-12-05 16:00:52 -0500

    fixed branch issues

---
+++ CameraGroup.js
@@ -0,0 +1,56 @@
+var CameraConnection = require('./CameraConnection.js');
+var ViewingConnection = require('./ViewingConnection.js');
+var events=require(events);
+CameraGroup.prototype=Object.create(EventEmitter.prototype);
+CameraGroup.prototype.constructor=CameraGroup;
+
+function CameraGroup(newGroupId, cameraSock){
+	var groupId = newGroupId;
+	var viewingClients = new Map();
+	var nextViewerId = 0;
+	var eventEmitter=new event.EventEmitter();
+
+
+	// ClientConnection object representing the camera uploading video
+	var cameraClient = new CameraClient(groupId,this,cameraSock);
+	cameraClient.send({connected: true});
+
+	this.addViewer = function(viewerSock){
+		console.log('New viewing client added to ' + this.description());
+		viewingClients.set(nextViewerId, new ViewingClient(groupId, this, viewerSock));
+		console.log('Sending ack message to new viewing client in ' + this.description());
+		viewerSock.send(JSON.stringify({"connected":true, "viewerId": nextViewerId}));
+		nextViewerId++;
+	};
+
+	this.description = function(){ return 'CameraGroup ' + groupId;};
+
+	this.onCameraMessage = function(message){
+		console.log(this.description() + ' received message from camera client');
+		sendMessageToViewer(message, message.viewerId);
+	};
+
+	this.sendMessageToViewer = function(message,viewerId){
+		console.log(this.description() + ' sending message to viewing client ' + viewerId);
+		viewingClients.get(viewerId).send(message);
+	};
+	
+	this.onViewerMessage = function(message){
+		console.log(this.description() + ' received message from viewing client');
+		console.log(this.description() + ' sending message to camera client');
+		cameraClient.sendMessage(message);
+	};
+
+	this.onMessage = function(message, isCamera){
+		console.log(message);
+		var signal = JSON.parse(message);
+		if(isCamera) onCameraMessage(signal);
+		else onViewerMessage(signal);
+	};
+
+	eventEmitter.on("disconnect", disconnectCameraGroup);
+
+	
+};
+
+module.exports = CameraGroup;
---
+++ ClientConnection.js
@@ -0,0 +1,27 @@
+function ClientConnection(newGroupId, newParent, newWebSock){
+	var groupId = newGroupId;
+	var parent = newParent;
+	var webSock = newWebSock;
+
+	this.isCamera = function(){
+		return false;
+	};
+
+	this.getGroupId = function(){
+		return groupId;
+	};
+
+	// add message handler function onMessage
+	webSock.on('message', onMessage);
+
+	// to do: add error handling, check for closed web sockets
+	this.sendMessage = function(message){
+		webSock.send(JSON.stringify(message));
+	};
+};
+
+ClientConnection.prototype.onMessage = function(message){
+	parent.onMessage(message, this.isCamera());
+};
+
+module.exports = ClientConnection;
