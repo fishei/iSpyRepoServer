c1f77be932c95b51ce15ec2003105135ce615496
fishei <fishei@rpi.edu>
2017-12-06 17:02:23 -0500

    server side bug fix in CameraConnection.js

--- CameraConnection.js
+++ CameraConnection.js
@@ -3,15 +3,15 @@ var ClientConnection = require('./ClientConnection');
 // child class for client connections to cameras
 function CameraConnection(newGroupId, newParent, newWebSock){
 	ClientConnection.call(this,newGroupId, newParent, newWebSock);
+
+	// override this method in camera client objects
+	this.isCamera = function(){
+		console.log('TESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSST!@');
+		return true;
+	};
 };
 
 CameraConnection.prototype = Object.create(ClientConnection.prototype);
 CameraConnection.prototype.constructor = CameraConnection;
-
-// override this method in camera client objects
-CameraConnection.prototype.isCamera = function(){
-	console.log('TESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSST!@');
-	return true;
-};
 
 module.exports = CameraConnection;
