ad9e553eae12b61d4abde4a6ba8853f151604d23
fishei <fishei@rpi.edu>
2017-12-06 19:20:02 -0500

    resolved merge conflicts

--- public/cameraClient.js
+++ public/cameraClient.js
@@ -1,24 +1,7 @@
 
-<<<<<<< HEAD
-var localVideo = null
-	,peerConnection = null
-	,localVideoStream = null
-	,hasSentAnswer = false
-	,startButton = null
-	,wsc = new WebSocket('wss://ispyrevolution.com/websocket/')
-	
-	// configure peer connections to use our turn server running on DO droplet
-	,peerConnectionConfig = {iceServers: 
-                         [{'urls': 'turn:ispyrevolution.com:3478'
-		  	  ,'username': 'duncan'
-			  ,'credential': 'thisisduncan'
-		  }
-	]};
-=======
 function CameraClient(){
 	var self = this;
 	ClientBase.call(this);
->>>>>>> ObjectOriented
 
 	this.viewingPeers = new Map();
 	this.offersSent = new Map();
@@ -90,72 +73,6 @@ function CameraClient(){
 	);			
 };
 
-<<<<<<< HEAD
-	// set up event handler to handle messages from signaling svr
-wsc.onmessage = function(evt){
-	console.log('received message');
-	if(!peerConnection) {
-		return;
-	}
-	var signal = JSON.parse(evt.data);
-	//check for session description message from signaling server
-	if(signal.sdp){
-		console.log('received SDP from remote peer');
-		peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp));
-	}
-	//check for ice candidate description from signaling server
-	else if(signal.candidate){
-		console.log("Received ICECandidate from remote peer.");
-		peerConnection.addIceCandidate(new RTCIceCandidate(signal.candidate));
-	}
-	//if we haven't replied to the caller then send them an answer
-	if(!hasSentAnswer) createAndSendAnswer();
-};
-
-function startStreaming(){
-	// to do: move getUserMedia options to "static variable" at top of page
-	console.log('starting stream upload');
-	//startButton.addAttribute("disabled");
-	//create a peer connection object and add an ice candidate handler to send ice candidates to remote peer
-	peerConnection = new RTCPeerConnection(peerConnectionConfig);
-	peerConnection.onicecandidate = onIceCandidateHandler;
-	//get video and audio feed from local device, add streams to peer connection and local video element
-	navigator.getUserMedia({"audio": false, "video": true}, function(stream){
-		console.log('retrieved local video stream');
-		localVideoStream = stream;
-		localVideo.src = URL.createObjectURL(localVideoStream);
-		peerConnection.addStream(localVideoStream);
-	}, function(error){console.log(error);});
-		console.log('added local video stream');
-};
-
-//create a session description, send it to remote peer, and set the local description of the peer connection
-function createAndSendAnswer(){
-	console.log('creating answer');
-	peerConnection.createAnswer(
-		function(answer){
-			console.log('created answer');
-			var ans = new RTCSessionDescription(answer);
-			peerConnection.setLocalDescription(new RTCSessionDescription(ans), 
-				function(){
-					console.log('set local description');
-					wsc.send(JSON.stringify({"sdp":ans}));
-				},
-				function(error){console.log(error);}
-			);
-		},
-		function(error){console.log(error);}
-	);
-	hasSentAnswer = true;
-};
-
-//fires when an ice candidate is received from the turn server, sends candidate description to remote peer
-function onIceCandidateHandler(evt) {
-  console.log('received ice candidate');
-  if (!evt || !evt.candidate) return;
-  console.log('sending ice candidate to remote peer');
-  wsc.send(JSON.stringify({"candidate": evt.candidate }));
-=======
 CameraClient.prototype = Object.create(ClientBase.prototype);
 CameraClient.constructor = CameraClient;
 
@@ -185,7 +102,6 @@ CameraClient.prototype.onDisconnectMessage = function(message){
 		this.getPeerConnection(message.viewerId).close();
 		this.viewingPeers.delete(message.viewerId);
 	}
->>>>>>> ObjectOriented
 };
 
 CameraClient.prototype.getClientType = function(){return 'camera';};
--- public/viewingClient.js
+++ public/viewingClient.js
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-var remoteVideo = null
-	,peerConnection = null
-	,remoteVideoStream = null
-	,wsc = new WebSocket('wss://ispyrevolution.com/websocket/')
-	//configure peer connection to use our turn server running on DO droplet
-	,peerConnectionConfig = {iceServers: 
-       [{	'urls': 'turn:ispyrevolution.com:3478'
-		  	  ,'username': 'duncan'
-			  ,'credential': 'thisisduncan'
-		  }
-	]};
-
-function pageReady(){
-	remoteVideo = document.getElementById("remoteVideo");
-	//create a peer connection and add handler functions
-	peerConnection = new RTCPeerConnection(peerConnectionConfig);
-	peerConnection.onicecandidate = onIceCandidateHandler;
-	peerConnection.onaddstream = onAddStreamHandler;
-	//create an offer and send it to camera client
-	wsc.onopen = createAndSendOffer;
-};
-
-function createAndSendOffer(){
-	console.log('creating offer');
-	peerConnection.createOffer(
-		function(offer){
-			console.log('created offer');
-			var off = new RTCSessionDescription(offer);
-			peerConnection.setLocalDescription(new RTCSessionDescription(off),
-				function(){
-					console.log('set local description, sending offer');
-					wsc.send(JSON.stringify({"sdp":off}));
-				},
-				function(error){console.log(error);}
-			);
-		},
-		function(error){console.log(error);},
-		{offerToReceiveVideo: 1} //this line got everything to work, viewing clients must offer to receive the streams the want from the turn server/ camera peer
-	);
-};
-
-wsc.onmessage = function(evt){
-	var signal = JSON.parse(evt.data);
-	//check for session description messages
-	if(signal.sdp){
-		console.log("Received SDP from remote peer.");
-		peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp));
-	}
-	//check for ice candidate messages
-	else if (signal.candidate) {
-		console.log("Received ICECandidate from remote peer.");
-		peerConnection.addIceCandidate(new RTCIceCandidate(signal.candidate));
-	}
-};
-
-//fires when an ice candidate is received from turn server, sends candidate description to remote peer
-function onIceCandidateHandler(evt) {
-  console.log('received ice candidate');
-  if (!evt || !evt.candidate) return;
-  console.log('sending ice candidate to remote peer');
-  wsc.send(JSON.stringify({"candidate": evt.candidate }));
-};
-
-//handles adding video stream from remote peer to video element on webpage
-function onAddStreamHandler(evt) { 
-	// set remote video stream as source for remote video HTML5 element
-	remoteVideo.src = URL.createObjectURL(evt.stream);
-=======
 function ViewingClient(){
 	ClientBase.call(this);
 	
@@ -77,7 +8,7 @@ function ViewingClient(){
 	this.initializePeerConnection = function(){
 		this.peerConn = new RTCPeerConnection(this.peerConnectionConfig);
 		self = this;
-		this.peerConn.onicecandidate = function(evt){
+		this.peerConn.onIceCandidate = function(evt){
 			console.log('received ice candidate');
 			console.log('sending ice candidate to remote peer');
 			self.wsc.send(JSON.stringify({
@@ -140,5 +71,4 @@ ViewingClient.onDisconnectMessage = function(message){
 	this.resetUIElements();
 	this.peerConn.close();
 	this.peerConn = null;
->>>>>>> ObjectOriented
 };
